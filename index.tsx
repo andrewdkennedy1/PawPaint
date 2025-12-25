aee3a290cc5b83e84e232ab6b9a9a7b32c84233e
andrew <andrew@bortonfruit.com>
2025-12-24 16:23:36 -0800

    Build Tailwind locally for old Safari

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 // Register Service Worker for offline support
 if ('serviceWorker' in navigator) {
